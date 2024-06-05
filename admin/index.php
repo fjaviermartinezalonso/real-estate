b0886e83013ee91b638a1aea9273815311c8a4e9
fjaviermartinezalonso <54067143+fjaviermartinezalonso@users.noreply.github.com>
2024-06-05 20:59:35 +0200

    Add property listing

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,13 @@
 <?php
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    // Leer base de datos
+    $query = "SELECT id, titulo, imagen, precio FROM propiedades";
+    $propiedades = mysqli_query($db, $query);
+
+
+
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -7,6 +16,32 @@
         <h1>Admin de BienesRaices</h1>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($propiedad = mysqli_fetch_assoc($propiedades)) {?>
+                <tr>
+                    <td> <?php echo $propiedad["id"] ?> </td>
+                    <td> <?php echo $propiedad["titulo"] ?> </td>
+                    <td> <img src="/images/" <?php echo $propiedad["imagen"] ?> alt="imagen propiedad"></td>
+                    <td> <?php echo $propiedad["precio"] ?>€ </td>
+                    <td>
+                        <a href="" class="boton-amarillo-block">Actualizar</a>
+                        <a href="" class="boton-rojo-block">Eliminar</a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </main>
 
 <?php
