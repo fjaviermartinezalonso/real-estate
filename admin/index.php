2a17b721b2b8e6afaa7bda9fc8fc7b9c825af129
fjaviermartinezalonso <54067143+fjaviermartinezalonso@users.noreply.github.com>
2024-06-09 18:43:38 +0200

    Implement remove property command

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,26 @@
     $query = "SELECT id, titulo, imagen, precio FROM propiedades";
     $propiedades = mysqli_query($db, $query);
 
+    // Si se pulsó el botón de Eliminar propiedad
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+        $id = filter_var($_POST["id"], FILTER_VALIDATE_INT);
 
+        if($id) {
+            // Eliminar imagen asociada de la base de datos
+            $query = "SELECT imagen FROM propiedades WHERE id = $id";
+            $resultado = mysqli_query($db, $query);
+            $imagenDB = mysqli_fetch_assoc($resultado)["imagen"];
+
+            $imageFolder = "../images/";
+            unlink($imageFolder . $imagenDB);
+            
+            // Eliminar registro de la tabla
+            $query = "DELETE FROM propiedades WHERE id = $id";
+            if(mysqli_query($db, $query)) { // si se logra recargamos página
+                header("location: /admin");
+            }
+        }
+    }
 
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -36,7 +55,10 @@
                     <td> <?php echo $propiedad["precio"] ?>€ </td>
                     <td>
                         <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad["id"]?>" class="boton-amarillo-block">Actualizar</a>
-                        <a href="admin/propiedades/borrar.php" class="boton-rojo-block">Eliminar</a>
+                        <form method="POST">
+                            <input type="hidden" name="id" value="<?php echo $propiedad["id"]; ?>">
+                            <button type="submit" class="boton-rojo-block w-100">Eliminar</a>
+                        </form>
                     </td>
                 </tr>
                 <?php } ?>
